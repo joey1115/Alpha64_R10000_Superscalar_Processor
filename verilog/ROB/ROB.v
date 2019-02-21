dc96e48ba9d68fbbdd3044f33535c53c0983e5fe
brianpur <brianpur@umich.edu>
2019-02-21 15:54:19 -0500

    edit ROB.v

--- verilog/ROB/ROB.v
+++ verilog/ROB/ROB.v
@@ -1,17 +1,66 @@
-module ROB_m (
-  input  ROB_PACKET_IN  rob_packet_in,
-  output ROB_PACKET_OUT rob_packet_out
+//READ T and T_old of head or tail.
+//WRITE T_old and T for Head or tail.
+//Increment head (oldest inst).
+//Increment tail (newer inst).
+module rob_m (
+  input clock, reset,
+  input logic ht,
+  input COMMAND cmd, //WRITE, READ
+  input increment,    //ADD T/H
+  input logic [$clog2(`NUM_PR)-1:0] T_in,    
+  input logic [$clog2(`NUM_PR)-1:0] T_old_in,
+  output logic [$clog2(`NUM_PR)-1:0] T_out,    
+  output logic [$clog2(`NUM_PR)-1:0] T_old_out,
+  output logic out
 );
 
-ROB_ENTRY_t [$clog2(`NUM_ROB)-1:0] ROB, next_ROB;
+  ROB_ENTRY_t [`NUM_ROB - 1:0] rob;
+  logic writeTail, readHead, readTail, incHead, incTail;
+  logic [$clog2(`NUM_ROB)-1: 0] headPointer, tailPointer; // not sure if register ok or need param
 
-// ROB logic
-  always_ff @(posedge clock) begin
+
+  always_ff @ (posedge clock) begin
     if(reset) begin
-      // ROB       <= `SD {`NUM_ROB{'{HT_FREE, PR_FREE}};
-    end else if(rob_packet_in.en) begin
-      ROB       <= `SD next_ROB;
-    end // if (if_id_enable)
-  end // always
+      headPointer = 0;
+      tailPointer = 0;
+      for(int i=0; i < `NUM_ROB; i++) begin
+        rob.valid = 0;
+      end
+    end
+    else begin
+      if(incTail)
+        tailPointer <= #`SD tailPointer + 1;
+      else
+        tailPointer <= #`SD tailPointer;
 
+      if(incHead)
+        headPointer <= #`SD headPointer + 1;
+      else
+        headPointer <= #`SD headPointer;
+
+      if(writeTail) begin
+        rob[tailPointer].T <= #`SD T_in;
+        rob[tailPointer].T_old <= #`SD T_old_in;
+      end
+    end
+  end
+
+  always_comb begin
+    writeTail = (cmd == WRITE) && ~ht;
+    readTail = (cmd == READ) && ~ht;
+    readHead = (cmd == READ) && ht;
+    incHead = increment && ht;
+    incTail = increment && ~ht;
+
+    if(readTail) begin
+      T_out = rob[tailPointer].T;
+      T_old_out = rob[tailPointer].T_old;
+      out = rob[tailPointer].valid;
+    end
+    else if (readHead) begin
+      T_out = rob[headPointer].T;
+      T_old_out = rob[headPointer].T_old;
+      out = rob[headPointer].valid;
+    end
+  end
 endmodule
