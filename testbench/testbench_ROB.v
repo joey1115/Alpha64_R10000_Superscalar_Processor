13a9337866d868902cdf6fa6712f9f91aa413c33
SPC91 <46912742+SPC91@users.noreply.github.com>
2019-02-23 14:40:34 -0500

    Deleted valid bits in ROB.v
    Valid is no more a part of ROB_ENTRY_t

--- testbench/testbench_ROB.v
+++ testbench/testbench_ROB.v
@@ -8,6 +8,40 @@
 //                                                                     //
 /////////////////////////////////////////////////////////////////////////
 
+/***** Quick Lookup *****
+
+`define NUM_PR                 64
+`define NUM_ROB                8
+
+typedef struct packed {
+  logic [$clog2(`NUM_PR)-1:0] T;
+  logic [$clog2(`NUM_PR)-1:0] T_old;
+} ROB_ENTRY_t;
+
+typedef struct packed {
+  logic [$clog2(`NUM_ROB)-1:0] head;
+  logic [$clog2(`NUM_ROB)-1:0] tail;
+  ROB_ENTRY_t [`NUM_ROB-1:0] entry;
+} ROB_t;
+
+typedef struct packed {
+  logic r;
+  logic inst_dispatch;
+  logic [$clog2(`NUM_PR)-1:0] T_in;
+  logic [$clog2(`NUM_PR)-1:0] T_old_in;
+} ROB_PACKET_IN;
+
+typedef struct packed {
+  logic [$clog2(`NUM_PR)-1:0] T_out;
+  logic [$clog2(`NUM_PR)-1:0] T_old_out;
+  logic out_correct;
+  logic struct_hazard;
+  logic [$clog2(`NUM_ROB)-1:0] head_idx_out;
+  logic [$clog2(`NUM_ROB)-1:0] ins_rob_idx;
+} ROB_PACKET_OUT;
+
+ ***********************/
+
 `timescale 1ns/100ps
 
 `include "sys_defs.vh"
