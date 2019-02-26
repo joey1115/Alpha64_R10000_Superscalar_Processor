5236f18c56eab8feb3cb9cae918ada44b268c7d0
brianpur <brianpur@umich.edu>
2019-02-26 15:01:10 -0500

    Update ROB.v
    make ROB.v neater a bit

--- verilog/ROB/ROB.v
+++ verilog/ROB/ROB.v
@@ -28,6 +28,32 @@ module ROB (
   logic [$clog2(`NUM_PR)-1:0] nextT, nextT_old;
   logic writeTail, moveHead;
 
+  always_comb begin
+    // condition for Retire
+    moveHead = (rob_packet_in.r) && en && rob.entry[rob.head].valid;
+    nextHeadPointer = (moveHead) ? (rob.head + 1) : rob.head;
+    nextHeadValid = (moveHead) ? 0 : rob.entry[rob.head].valid;
+    // condition for Dispatch
+    writeTail = (rob_packet_in.inst_dispatch) && en && (!rob_packet_out.struct_hazard || rob_packet_in.r);
+
+    // next state logic
+    nextTailPointer = (writeTail) ? (rob.tail + 1) : rob.tail;
+    nextT = (writeTail) ? rob_packet_in.T_in : rob.entry[rob.tail].T;
+    nextT_old = (writeTail) ? rob_packet_in.T_old_in : rob.entry[rob.tail].T_old;
+    nextTailValid = (writeTail) ? 1 : rob.entry[rob.tail].valid;
+
+    //outputs
+    rob_packet_out.out_correct = rob.entry[rob.tail - 1].valid;
+    rob_packet_out.ins_rob_idx = rob.tail - 1;
+    rob_packet_out.T_out = rob.entry[rob.tail - 1].T;
+    rob_packet_out.T_old_out = rob.entry[rob.tail - 1].T_old;
+    // output for Complete
+    rob_packet_out.head_idx_out = rob.head;
+    // output for Dispatch
+    rob_packet_out.struct_hazard = rob.entry[rob.tail].valid;
+
+  end
+
   always_ff @ (posedge clock) begin
     if(reset) begin
       rob.tail <= `SD 0;
@@ -54,12 +80,12 @@ module ROB (
             rob.entry[i].valid <= `SD 0;
         end
       end
-      //update head to allow retire
+      //update head to allow retire while flushing
       rob.head <= `SD nextHeadPointer;
       rob.entry[rob.head].valid <= `SD nextHeadValid;
       //move tail index to after branch
       rob.tail <= `SD rob_packet_in.flush_branch_idx + 1;
-    end
+    end // else if(rob_packet_in.branch_mispredict)
     else begin
       rob.tail <= `SD nextTailPointer;
       rob.head <= `SD nextHeadPointer;
@@ -67,6 +93,7 @@ module ROB (
       rob.entry[rob.tail].T <= `SD nextT;
       rob.entry[rob.tail].T_old <= `SD nextT_old;
       
+      // check is the tail and head index is same or different
       if (rob.tail != rob.head) begin
         rob.entry[rob.head].valid <= `SD nextHeadValid;
         rob.entry[rob.tail].valid <= `SD nextTailValid;
@@ -76,26 +103,4 @@ module ROB (
     end // if (reset) else
   end // always_ff
 
-  always_comb begin
-    // for Retire
-    moveHead = (rob_packet_in.r) && en && rob.entry[rob.head].valid;
-    nextHeadPointer = (moveHead) ? (rob.head + 1) : rob.head;
-    nextHeadValid = (moveHead) ? 0 : rob.entry[rob.head].valid;
-    // for Complete
-    rob_packet_out.head_idx_out = rob.head;
-    // for Dispatch
-    rob_packet_out.struct_hazard = rob.entry[rob.tail].valid;
-    writeTail = (rob_packet_in.inst_dispatch) && en && (!rob_packet_out.struct_hazard || rob_packet_in.r);
-
-    nextTailPointer = (writeTail) ? (rob.tail + 1) : rob.tail;
-    nextT = (writeTail) ? rob_packet_in.T_in : rob.entry[rob.tail].T;
-    nextT_old = (writeTail) ? rob_packet_in.T_old_in : rob.entry[rob.tail].T_old;
-    nextTailValid = (writeTail) ? 1 : rob.entry[rob.tail].valid;
-
-    rob_packet_out.out_correct = rob.entry[rob.tail - 1].valid;
-    rob_packet_out.ins_rob_idx = rob.tail - 1;
-    rob_packet_out.T_out = rob.entry[rob.tail - 1].T;
-    rob_packet_out.T_old_out = rob.entry[rob.tail - 1].T_old;
-
-  end
 endmodule
