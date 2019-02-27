9f3000584357fbf498155a8313c8dd6a3ddc0af8
SPC91 <46912742+SPC91@users.noreply.github.com>
2019-02-26 21:47:15 -0500

    Added more test cases for branch exception

--- testbench/testbench_ROB.v
+++ testbench/testbench_ROB.v
@@ -72,7 +72,7 @@ module testbench_ROB;
   logic [31:0] cycle_count;
 
   // Test Cases #1: Lecture Slides
-  `define TEST1_LEN 35
+  `define TEST1_LEN 46
   ROB_PACKET_IN  [`TEST1_LEN-1:0] test1;
   // solutions have one more state than test cases
   ROB_PACKET_OUT   [`TEST1_LEN:0] test1_out;
@@ -143,17 +143,12 @@ module testbench_ROB;
       cycle_count <= `SD (cycle_count + 1);
   end
 
-  // Check correctness
-  always @(negedge clock) begin
-    if (cycle_count >= 0 && cycle_count <= `TEST1_LEN)
-      check_Sol(cycle_count, rob_packet_out, test1_out[cycle_count]);
-  end
 
   initial begin
 
     /***** Test Case #1 *****/
 
-    // test1[c]: input at cycle c
+    // test1[c]: input at cycle c, the corresponding output will be at cycle c+1
     // {r, inst_dispatch, T_in, T_old_in, flush_branch_idx, branch_mispredict}
     // Cycle 0-6: simulates Lecture Slides with stalls added
     test1[0]  = '{0, 1,  5,  2, 0, 0};
@@ -197,7 +192,18 @@ module testbench_ROB;
     test1[31] = '{0, 0, 12, 22, 4, 1}; // flush right after flush
     test1[32] = '{0, 1, 12, 22, 4, 0}; // write something after flush
     test1[33] = '{1, 1, 13, 23, 3, 1}; // flush and retire at the same time
-    test1[34] = '{1, 0, 13, 23, 3, 1}; // flush again at the same spot, and flush
+    test1[34] = '{1, 0, 13, 23, 3, 1}; // flush again at the same spot, and retire
+    // Cycle 35-43: reset and fill up the ROB and flush the head
+    test1[35] = '{0, 1, 10, 20, 0, 0}; // reset here
+    test1[36] = '{0, 1,  0, 10, 0, 0};
+    test1[37] = '{0, 1,  1, 11, 0, 0};
+    test1[38] = '{0, 1,  2, 12, 0, 0};
+    test1[39] = '{0, 1,  3, 13, 0, 0};
+    test1[40] = '{0, 1,  4, 14, 0, 0};
+    test1[41] = '{0, 1,  5, 15, 0, 0};
+    test1[42] = '{0, 1,  6, 16, 0, 0};
+    test1[43] = '{0, 1,  7, 17, 0, 0};
+    test1[44] = '{0, 0, 13, 23, 0, 1}; // flush at index 0 (head)
 
     // test1_out[c]: output at cycle c (test1[c]'s effect is shown by test1_out[c+1])
     // {T_out, T_old_out, out_correct, struct_hazard, head_idx_out, ins_rob_idx}
@@ -246,6 +252,17 @@ module testbench_ROB;
     test1_out[33] = '{12, 22, 1, 0, 2, 5};
     test1_out[34] = '{ 3, 13, 1, 0, 3, 3};
     test1_out[35] = '{ 3, 13, 0, 0, 4, 3};
+    // reset and fill up the ROB and flush the head
+    test1_out[36] = '{10, 20, 0, 0, 0, 0}; // reset here
+    test1_out[37] = '{ 0, 10, 1, 0, 0, 0};
+    test1_out[38] = '{ 1, 11, 1, 0, 0, 1};
+    test1_out[39] = '{ 2, 12, 1, 0, 0, 2};
+    test1_out[40] = '{ 3, 13, 1, 0, 0, 3};
+    test1_out[41] = '{ 4, 14, 1, 0, 0, 4};
+    test1_out[42] = '{ 5, 15, 1, 0, 0, 5};
+    test1_out[43] = '{ 6, 16, 1, 0, 0, 6};
+    test1_out[44] = '{ 7, 17, 1, 1, 0, 7};
+    test1_out[45] = '{ 7, 17, 1, 0, 0, 0};
 
 
     // Reset
@@ -256,40 +273,62 @@ module testbench_ROB;
     @(negedge clock);
     reset = 1'b1;
     
-    // Cycle 0 input
+    // Cycle 0
     @(negedge clock);
     reset = 1'b0;
+    check_Sol(cycle_count, rob_packet_out, test1_out[cycle_count]);
     rob_packet_in = test1[0];
 
-    // Cycle 1 input
+    // Cycle 1
     @(negedge clock);
-    reset = 1'b0;
+    check_Sol(cycle_count, rob_packet_out, test1_out[cycle_count]);
     rob_packet_in = test1[1];
 
-    // Cycle 2 input
+    // Cycle 2
     @(negedge clock);
+    check_Sol(cycle_count, rob_packet_out, test1_out[cycle_count]);
     rob_packet_in = test1[2];
 
-    // Cycle 3 input
+    // Cycle 3
     @(negedge clock);
+    check_Sol(cycle_count, rob_packet_out, test1_out[cycle_count]);
     rob_packet_in = test1[3];
 
-    // Cycle 4 input
+    // Cycle 4
     @(negedge clock);
+    check_Sol(cycle_count, rob_packet_out, test1_out[cycle_count]);
     en = 1'b0;
     rob_packet_in = test1[4];
 
-    // Cycle 5 input
+    // Cycle 5
     @(negedge clock);
+    check_Sol(cycle_count, rob_packet_out, test1_out[cycle_count]);
     en = 1'b1;
     rob_packet_in = test1[5];
 
-    // Cycle 6 input
-    @(negedge clock);
-    rob_packet_in = test1[6];
-
-    for (int i = 7; i < `TEST1_LEN; i=i+1) begin
+    for (int i = 6; i < 35; i=i+1) begin
       @(negedge clock);
+      check_Sol(cycle_count, rob_packet_out, test1_out[cycle_count]);
+      rob_packet_in = test1[i];
+    end
+
+    // Cycle 35
+    @(negedge clock);
+    check_Sol(cycle_count, rob_packet_out, test1_out[cycle_count]);
+    reset = 1'b1;
+    rob_packet_in = test1[35];
+
+    // Cycle 36: Reset takes effect
+    @(negedge clock);
+    //$display("Reset");
+    cycle_count = 36;
+    check_Sol(cycle_count, rob_packet_out, test1_out[cycle_count]);
+    reset = 1'b0;
+    rob_packet_in = test1[36];
+    
+    for (int i = 37; i < `TEST1_LEN; i=i+1) begin
+      @(negedge clock);
+      check_Sol(cycle_count, rob_packet_out, test1_out[cycle_count]);
       rob_packet_in = test1[i];
     end
 
