67ca0e2e0be7bb36cdfd2e38872f406ebb4a8575
Zhewei Wang <wzhewei@umich.edu>
2019-02-27 18:41:34 -0500

    Update submit.sh

--- submit.sh
+++ submit.sh
@@ -2,8 +2,4 @@ cd ./verilog/ROB/
 make nuke &> /dev/null
 cd ./../../
 make nuke &> /dev/null
-if [[ -d ./.git ]]
-then
-  rm -rf ./.git/
-fi
 /afs/umich.edu/user/j/i/jieltan/Public/470submit -p4 ./../$(basename "$PWD")
