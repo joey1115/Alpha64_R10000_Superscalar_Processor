faf33451a869957e36d5ee664b5a40d39c4a6d1b
Zhewei Wang <wzhewei@umich.edu>
2019-03-03 14:11:48 -0500

    Minor fix

--- verilog/RS/RS.v
+++ verilog/RS/RS.v
@@ -5,54 +5,53 @@ module RS (
 );
 
   RS_ENTRY_t [`NUM_FU-1:0] RS, next_RS;
-  FU_t       [`NUM_FU-1:0] FU_list = `FU_LIST;
-  logic      [`NUM_FU-1:0] T1_ready;
-  logic      [`NUM_FU-1:0] T2_ready;
-  logic      [`NUM_FU-1:0] RS_entry_ready;
-  logic                    dispatched;
+  FU_t       [`NUM_FU-1:0] FU_list = `FU_LIST; // List of FU
+  logic      [`NUM_FU-1:0] T1_ready;           // If T1 is ready
+  logic      [`NUM_FU-1:0] T2_ready;           // If T2 is ready
+  logic      [`NUM_FU-1:0] RS_entry_ready;     // If a RS entry is ready
+  logic                    dispatched;         // If a inst has been dispatched to a previously free entry
   assign rs_packet_out.RS = next_RS;
 
   // Hazard
   always_comb begin
     rs_packet_out.valid = `FALSE;
     for (int i = 0; i < `NUM_FU; i++) begin
-      T1_ready[i]       = RS[i].T1.ready || RS[i].T1.idx == rs_packet_in.CDB_T; // T1 is ready or updated by CDB
-      T2_ready[i]       = RS[i].T2.ready || RS[i].T2.idx == rs_packet_in.CDB_T; // T2 is ready or updated by CDB
-      RS_entry_ready[i] = T1_ready[i] && T2_ready[i] || RS[i].busy == `FALSE;   // T1 and T2 are ready to issue
-      if ( RS_entry_ready[i] && FU_list[i] == rs_packet_in.FU) begin            // FU match
-        rs_packet_out.valid = `TRUE;                                            // No hazard
+      T1_ready[i]       = RS[i].T1.ready || RS[i].T1.idx == rs_packet_in.CDB_T;                    // T1 is ready or updated by CDB
+      T2_ready[i]       = RS[i].T2.ready || RS[i].T2.idx == rs_packet_in.CDB_T;                    // T2 is ready or updated by CDB
+      RS_entry_ready[i] = T1_ready[i] && T2_ready[i];                                              // T1 and T2 are ready to issue
+      if ( ( RS_entry_ready[i]  || RS[i].busy == `FALSE ) && FU_list[i] == rs_packet_in.FU ) begin // FU match
+        rs_packet_out.valid = `TRUE;                                                               // No hazard
         break;
-      end
-    end
-  end
+      end // if ( ( RS_entry_ready[i]  || RS[i].busy == `FALSE ) && FU_list[i] == rs_packet_in.FU ) begin
+    end // for (int i = 0; i < `NUM_FU; i++) begin
+  end // always_comb begin
 
   always_comb begin
     next_RS = RS;
+    rs_packet_out.FU_packet_out = `FU_RESET;
 
-    // Complete
     for (int i = 0; i < `NUM_FU; i++) begin
+
+      // Complete
       if ( rs_packet_in.complete_en ) begin             // CDB ready to update
         if ( RS[i].T1.idx == rs_packet_in.CDB_T ) begin // T1 idx match
           next_RS[i].T1.ready = `TRUE;                  // T1 ready
-        end
+        end // if ( RS[i].T1.idx == rs_packet_in.CDB_T ) begin
         if ( RS[i].T2.idx == rs_packet_in.CDB_T ) begin // T1 idx match
           next_RS[i].T2.ready = `TRUE;                  // T2 ready
-        end
-      end
-    end
+        end // if ( RS[i].T2.idx == rs_packet_in.CDB_T ) begin
+      end // if ( rs_packet_in.complete_en ) begin
 
-    // Issue
-    for (int i = 0; i < `NUM_FU; i++) begin
-      if ( RS_entry_ready[i] ) begin                           // T1 and T2 are ready to issue
-        rs_packet_out.FU_packet_out[i].ready  = `TRUE;         // Ready to issue
-        rs_packet_out.FU_packet_out[i].T_idx  = RS[i].T_idx;   // Output T_idx
-        rs_packet_out.FU_packet_out[i].T1_idx = RS[i].T1.idx;  // Output T1_idx
-        rs_packet_out.FU_packet_out[i].T2_idx = RS[i].T2.idx;  // Output T2_idx
-        next_RS[i] = '{`FALSE, `ZERO_REG, `T_RESET, `T_RESET}; // Clear RS entry
-      end else begin
-        rs_packet_out.FU_packet_out[i] = '{`FALSE, `ZERO_REG, `ZERO_REG, `ZERO_REG}; // Output not ready
-      end
-    end
+      // Issue
+      if ( RS_entry_ready[i] ) begin                                                 // T1 and T2 are ready to issue
+        rs_packet_out.FU_packet_out[i].ready  = `TRUE;                               // Ready to issue
+        rs_packet_out.FU_packet_out[i].T_idx  = RS[i].T_idx;                         // Output T_idx
+        rs_packet_out.FU_packet_out[i].T1_idx = RS[i].T1.idx;                        // Output T1_idx
+        rs_packet_out.FU_packet_out[i].T2_idx = RS[i].T2.idx;                        // Output T2_idx
+        next_RS[i] = '{`FALSE, `ZERO_REG, `T_RESET, `T_RESET};                       // Clear RS entry
+      end // if ( RS_entry_ready[i] ) begin
+
+    end // for (int i = 0; i < `NUM_FU; i++) begin
 
     dispatched = `FALSE;
     //Dispatch
@@ -76,7 +75,7 @@ module RS (
     end
 
     // Dispatch
-    if ( dispatched == `FALSE ) begin                                                 // If inst has not been dispatched
+    if ( dispatched == `FALSE ) begin                                                                           // If inst has not been dispatched
       for (int i = 0; i < `NUM_FU; i++) begin
         if ( ( next_RS[i].busy == `FALSE ) && rs_packet_in.dispatch_en && rs_packet_in.FU == FU_list[i] ) begin // If previous inst left
           next_RS[i].busy  = `TRUE;                                                                             // RS entry busy
@@ -84,11 +83,11 @@ module RS (
           next_RS[i].T1    = rs_packet_in.T1;                                                                   // Write T1
           next_RS[i].T2    = rs_packet_in.T2;                                                                   // Write T2
           break;
-        end
-      end
-    end
+        end // if ( ( next_RS[i].busy == `FALSE ) && rs_packet_in.dispatch_en && rs_packet_in.FU == FU_list[i] ) begin
+      end // for (int i = 0; i < `NUM_FU; i++) begin
+    end // if ( dispatched == `FALSE ) begin
 
-  end
+  end // always_comb begin
 
 // ROB logic
   always_ff @(posedge clock) begin
@@ -96,7 +95,7 @@ module RS (
       RS <= `SD `RS_RESET;
     end else if(en) begin
       RS <= `SD next_RS;
-    end // if (f_d_enable)
+    end // else if(en) begin
   end // always
 
 endmodule
--- verilog/RS/RS.vh
+++ verilog/RS/RS.vh
@@ -6,53 +6,52 @@
 `include "../../sys_defs.vh"
 
 typedef struct packed {
-  logic [$clog2(`NUM_PR)-1:0] idx;
-  logic                       ready;
+  logic [$clog2(`NUM_PR)-1:0] idx;   // T idx
+  logic                       ready; // T plus
 } T_t;
 
 typedef struct packed {
-  // FU_t                        FU;
-  logic                       busy;
-  // logic [5:0]                 op;
-  logic [$clog2(`NUM_PR)-1:0] T_idx;
-  T_t                         T1;
-  T_t                         T2;
+  logic                       busy;  // RS entry busy
+  logic [$clog2(`NUM_PR)-1:0] T_idx; // Dest idx
+  T_t                         T1;    // T1
+  T_t                         T2;    // T2
 } RS_ENTRY_t;
 
 `define FU_LIST '{ \
-  {`NUM_ALU{FU_ALU}}, \
-  {`NUM_MULT{FU_MULT}}, \
-  {`NUM_ST{FU_ST}}, \
-  {`NUM_LD{FU_LD}} \
+  {`NUM_ALU{FU_ALU}}, \   // ALU
+  {`NUM_MULT{FU_MULT}}, \ // MULT
+  {`NUM_ST{FU_ST}}, \     // ST
+  {`NUM_LD{FU_LD}} \      // LD
 }
 
-`define T_RESET {`ZERO_REG, 1'b0}
-`define RS_ENTRY_RESET  {`FALSE, `ZERO_REG, `T_RESET, `T_RESET}
-`define RS_RESET '{`NUM_FU{`RS_ENTRY_RESET}}
+`define T_RESET {`ZERO_REG, 1'b0}                                // T reset
+`define RS_ENTRY_RESET  {`FALSE, `ZERO_REG, `T_RESET, `T_RESET}  // RS entry reset
+`define RS_RESET '{`NUM_FU{`RS_ENTRY_RESET}}                     // RS reset
 
 typedef struct packed {
-  logic [$clog2(`NUM_PR)-1:0] dest_idx;
-  T_t                         T1;
-  T_t                         T2;
-  logic                       complete_en;
-  // logic [`NUM_FU-1:0]         FU_ready_list;
-  logic                       dispatch_en;
-  FU_t                        FU;
-  logic [$clog2(`NUM_PR)-1:0] CDB_T;
+  logic [$clog2(`NUM_PR)-1:0] dest_idx;    // Dest idx
+  T_t                         T1;          // T1
+  T_t                         T2;          // T2
+  logic                       complete_en; // If CDB is ready
+  logic                       dispatch_en; // If can be dispatched
+  FU_t                        FU;          // Required FU
+  logic [$clog2(`NUM_PR)-1:0] CDB_T;       // CDB tag
 } RS_PACKET_IN;
 
 typedef struct packed {
-  logic                       ready;
-  logic [$clog2(`NUM_PR)-1:0] T_idx;
-  logic [$clog2(`NUM_PR)-1:0] T1_idx;
-  logic [$clog2(`NUM_PR)-1:0] T2_idx;
+  logic                       ready;  // If an entry is ready
+  logic [$clog2(`NUM_PR)-1:0] T_idx;  // Dest idx
+  logic [$clog2(`NUM_PR)-1:0] T1_idx; // T1 idx
+  logic [$clog2(`NUM_PR)-1:0] T2_idx; // T2 idx
 } FU_PACKET_t;
 
+`define FU_ENTRY_RESET {`FALSE, `ZERO_REG, `ZERO_REG, `ZERO_REG} // FU entry reset
+`define FU_RESET '{`NUM_FU{`FU_ENTRY_RESET}}                     // FU reset
+
 typedef struct packed {
-  logic                             valid;
-  // logic       [$clog2(`NUM_FU)-1:0] FU_idx;
-  FU_PACKET_t [`NUM_FU-1:0]         FU_packet_out;
-  RS_ENTRY_t  [`NUM_FU-1:0]         RS;
+  logic                             valid;         // valid is true if no hazard
+  FU_PACKET_t [`NUM_FU-1:0]         FU_packet_out; // List of output fu
+  RS_ENTRY_t  [`NUM_FU-1:0]         RS;            // RS
 } RS_PACKET_OUT;
 
 `endif
