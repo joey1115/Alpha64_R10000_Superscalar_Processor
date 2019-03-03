1e4582fc0a3933da13b49e4e4cb3348545b0a0a6
Zhewei Wang <wzhewei@umich.edu>
2019-03-03 14:23:59 -0500

    Minor fix

--- verilog/RS/RS.v
+++ verilog/RS/RS.v
@@ -52,10 +52,7 @@ module RS (
         next_RS[i] = '{`FALSE, `ZERO_REG, `T_RESET, `T_RESET};                       // Clear RS entry
       end // if ( RS_entry_ready[i] ) begin
 
-    end // for (int i = 0; i < `NUM_FU; i++) begin
-
-    //Dispatch
-    for (int i = 0; i < `NUM_FU; i++) begin
+      //Dispatch
       if ( RS_entry_empty[i] && FU_list[i] == rs_packet_in.FU && rs_packet_in.dispatch_en ) begin // RS entry was not busy and inst ready to dispatch and FU match
         next_RS[i].busy  = `TRUE;                                                                 // RS entry busy
         next_RS[i].T_idx = rs_packet_in.dest_idx;                                                 // Write T
@@ -63,7 +60,8 @@ module RS (
         next_RS[i].T2    = rs_packet_in.T2;                                                       // Write T2
         break;
       end // if ( RS[i].busy == `FALSE && rs_packet_in.dispatch_en ) begin
-    end
+
+    end // for (int i = 0; i < `NUM_FU; i++) begin
 
   end // always_comb begin
 
