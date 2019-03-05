e98ec9b0591d5ec3a4fdc138359c8df18872d378
Zhewei Wang <wzhewei@umich.edu>
2019-03-05 16:56:00 -0500

    Update case

---
+++ verilog/map_table/Map_Table.v
@@ -0,0 +1,56 @@
+/*
+Input:
+Dispatch rega, regb, reg_dest;
+CDB.T from CDB;
+T from Free list;
+Registers from Arch Map;
+Output:
+T1 & T2 to Reservation Stations;
+Told to ROB;
+*/
+// the size of maptable
+// definition of input and output
+// output T1 T2 to RS
+// output Told to ROB
+`timescale 1ns/100ps
+module Map_Table (
+  input  en, clock, reset,
+  input  MAP_TABLE_PACKET_IN  map_table_packet_in,
+  output MAP_TABLE_PACKET_OUT map_table_packet_out
+);
+
+  MAP_TABLE_t [`NUM_MAP_TABLE-1:0] map_table, next_map_table;
+  logic i;
+
+// ROB logic
+  always_ff @(posedge clock) begin
+    if(reset) begin
+      for(int i=0; i < `NUM_MAP_TABLE; i++) begin
+        map_table[i] = '{ i, `TRUE};                  
+      end
+    end else if(en) begin
+      map_table <= `SD next_map_table;
+    end // if (f_d_enable)
+  end // always
+
+  always_comb begin
+    next_map_table = map_table;
+    if (map_table_packet_in.Dispatch_enble && en) begin       // no dispatch hazard
+      next_map_table[map_table_packet_in.Dispatch_reg_dest] = '{map_table_packet_in.Freelist_T, `FALSE}; 
+                                                             //renew maptable from freelist but not ready yet
+    end
+    if (map_table_packet_in.CDB_enable && en) begin
+      // genvar i;
+      for ( int i=0; i< `NUM_MAP_TABLE;i++) begin  
+        if (map_table[i].PR_idx == map_table_packet_in.CDB_T) begin              // if CDB_T is the same as maptable value
+          next_map_table[i].T_PLUS_STATUS = `TRUE;           // The Tag in maptable change to ready
+          break;
+        end
+      end
+    end
+  end
+
+  assign map_table_packet_out.Told_to_ROB = map_table[map_table_packet_in.Dispatch_reg_dest].PR_idx;
+  assign map_table_packet_out.T1_to_RS    = map_table[map_table_packet_in.Dispatch_reg_T1];
+  assign map_table_packet_out.T2_to_RS    = map_table[map_table_packet_in.Dispatch_reg_T2];
+endmodule
---
+++ verilog/map_table/Map_Table.vh
@@ -0,0 +1,28 @@
+`ifndef __MAP_TABLE__
+`define __MAP_TABLE__
+
+`include "../../sys_config.vh"
+`include "../../sys_def.vh"
+
+typedef struct packed {
+  logic [$clog2(`NUM_PR)-1:0] PR_idx;            //PR index
+  logic                       T_PLUS_STATUS;     //Tag plus state
+} MAP_TABLE_t;
+
+typedef struct packed {
+  logic                       Dispatch_enable;    // no Dispatch hazard
+  logic [4:0]                 Dispatch_reg_dest;  // reg from dispatch
+  logic [4:0]                 Dispatch_reg_T1;
+  logic [4:0]                 Dispatch_reg_T2;
+  logic [$clog2(`NUM_PR)-1:0] Freelist_T;         // tags from freelist
+  logic [$clog2(`NUM_PR)-1:0] CDB_T;              // broadcast from CDB
+  logic                       CDB_enable;         //CDB enable
+} MAP_TABLE_PACKET_IN;
+
+typedef struct packed {
+  logic [$clog2(`NUM_PR)-1:0] Told_to_ROB;       // output Told to ROB
+  MAP_TABLE_t                 T1_to_RS;
+  MAP_TABLE_t                 T2_to_RS;
+} MAP_TABLE_PACKET_OUT;
+
+`endif
