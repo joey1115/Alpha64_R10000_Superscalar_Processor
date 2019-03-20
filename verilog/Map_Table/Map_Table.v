e1a116d7ea18822a4e74cde1606c309ccf8639d3
Zhewei Wang <wzhewei@umich.edu>
2019-03-20 04:59:55 -0400

    Minor fix

--- verilog/Map_Table/Map_Table.v
+++ verilog/Map_Table/Map_Table.v
@@ -67,9 +67,9 @@ module Map_Table (
       next_map_table[map_table_packet_in.dest_idx] = '{map_table_packet_in.T_idx, `FALSE};     //renew maptable from freelist but not ready yet
       next_map_table[31].ready = `TRUE;
       next_backup_map_table[map_table_packet_in.ROB_tail_idx] = next_map_table;                              //backup the map
-      // for (int i=0; i<32;i++) begin
-      //   backup_map_table[map_table_packet_in.tail_idx][i].ready = `TRUE;                        // ready all the bit
-      // end
+      for (int i=0; i<32;i++) begin
+        backup_map_table[map_table_packet_in.tail_idx][i].ready = `TRUE;                        // ready all the bit
+      end
     end
   end
 
