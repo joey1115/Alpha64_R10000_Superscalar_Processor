debd638d97a0819327ec55b779f0b3aa24160ae6
Zhewei Wang <wzhewei@umich.edu>
2019-03-21 00:55:52 -0400

    Update Map_Table

--- verilog/Map_Table/Map_Table.v
+++ verilog/Map_Table/Map_Table.v
@@ -15,7 +15,7 @@
  * output: T_old to ROB;
  * 
  * (2) send T1 (and ready bit) and T2 (and ready bit) to RS
- * input: reg_a and reg_b from decoder
+ * input: reg_a_idx and reg_b_idx from decoder
  * output: T1 and T1_r, T2 and T2_r to RS;
  * 
  ***********************************************************/
@@ -23,21 +23,49 @@
 `timescale 1ns/100ps
 
 module Map_Table (
-  input  en, clock, reset,
+  input  logic                en, clock, reset,
   input  MAP_TABLE_PACKET_IN  map_table_packet_in,
-
 `ifndef SYNTH_TEST
-  output T_t [31:0] map_table_out,
+  output T_t [31:0]           map_table_out,
 `endif
   output MAP_TABLE_PACKET_OUT map_table_packet_out
 );
 
+  T_t                       T1, T2;
   T_t [31:0]                map_table, next_map_table;
   T_t [`NUM_ROB-1:0][31:0]  backup_map_table, next_backup_map_table;
    
 `ifndef SYNTH_TEST
-  assign map_table_out = map_table;
+  assign map_table_out                 = map_table;
 `endif
+  assign map_table_packet_out.Told_idx = map_table[map_table_packet_in.reg_dest].idx;
+  assign T1                            = map_table[map_table_packet_in.reg_a_idx];
+  assign T2                            = map_table[map_table_packet_in.reg_b_idx];
+  assign map_table_packet_out.T1.idx   = T1.idx;
+  assign map_table_packet_out.T1.ready = ( map_table_packet_in.CDB_en && T1.idx == map_table_packet_in.CDB_T_idx ) || T1.ready;
+  assign map_table_packet_out.T2.idx   = T2.idx;
+  assign map_table_packet_out.T2.ready = ( map_table_packet_in.CDB_en && T2.idx == map_table_packet_in.CDB_T_idx ) || T2.ready;
+
+  always_comb begin
+    next_map_table = map_table;
+    // Rollback
+    if (map_table_packet_in.rollback_en) begin
+      next_map_table = backup_map_table[map_table_packet_in.ROB_rollback_idx];
+    end
+    // CDB_T updata ready
+    if ( map_table_packet_in.CDB_en && map_table[map_table_packet_in.CDB_dest_idx].idx == map_table_packet_in.CDB_T_idx ) begin
+      next_map_table[map_table_packet_in.CDB_dest_idx].ready = `TRUE;
+    end
+    // PR update T_idx
+    if ( map_table_packet_in.Dispatch_en ) begin                                                      // no dispatch hazard
+      next_map_table[map_table_packet_in.dest_idx]            = '{map_table_packet_in.T_idx, `FALSE}; // renew maptable from freelist but not ready yet
+      next_map_table[31].ready                                = `TRUE;                                // Force ZERO_REG to be rady
+      next_backup_map_table[map_table_packet_in.ROB_tail_idx] = next_map_table;                       // backup the map
+      for (int i=0; i<32;i++) begin
+        backup_map_table[map_table_packet_in.tail_idx][i].ready = `TRUE;                              // ready all the bit
+      end
+    end
+  end
 
   always_ff @(posedge clock) begin
     if(reset) begin
@@ -49,27 +77,4 @@ module Map_Table (
     end
   end
 
-  always_comb begin
-    next_map_table = map_table;
-    // Rollback
-    if (map_table_packet_in.rollback_en) begin
-      next_map_table = backup_map_table[map_table_packet_in.ROB_rollback_idx];
-    end
-    // CDB_T updata ready
-    if ( map_table_packet_in.CDB_en && map_table[map_table_packet_in.CDB_dest_idx].idx == map_table_packet_in.CDB_T_idx ) begin
-      map_table[map_table_packet_in.CDB_dest_idx].ready = `TRUE;
-    end
-    // PR updata T_idx
-    if ( map_table_packet_in.Dispatch_en ) begin   // no dispatch hazard
-      next_map_table[map_table_packet_in.dest_idx] = '{map_table_packet_in.T_idx, `FALSE};     //renew maptable from freelist but not ready yet
-      next_map_table[31].ready = `TRUE;
-      next_backup_map_table[map_table_packet_in.ROB_tail_idx] = next_map_table;                              //backup the map
-      for (int i=0; i<32;i++) begin
-        backup_map_table[map_table_packet_in.tail_idx][i].ready = `TRUE;                        // ready all the bit
-      end
-    end
-  end
-  assign map_table_packet_out.Told_idx = map_table[map_table_packet_in.reg_dest].idx;
-  assign map_table_packet_out.T1       = map_table[map_table_packet_in.reg_a];
-  assign map_table_packet_out.T2       = map_table[map_table_packet_in.reg_b];
 endmodule
