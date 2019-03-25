1accf83a18e77f2f77f80ba6bf144264bc0f3b35
zjoey <wzhewei@umich.edu>
2019-03-25 14:45:55 -0400

    Update Arch Map

--- verilog/Arch_Map/Arch_Map.v
+++ verilog/Arch_Map/Arch_Map.v
@@ -3,25 +3,25 @@
 `timescale 1ns/100ps
 
 module Arch_Map (
-  input  logic                     en, clock, reset,
-  input  logic                     retire_en,    // retire signal from ROB
+  input  logic                                          en, clock, reset,
+  input  logic                                          retire_en,    // retire signal from ROB
 `ifdef SYNTH_TEST
-  input  ROB_ARCH_MAP_OUT_t        ROB_Arch_Map_out
+  input  ROB_ARCH_MAP_OUT_t                             ROB_Arch_Map_out
 `else
-  input  ROB_ARCH_MAP_OUT_t        ROB_Arch_Map_out,
-  output ARCH_MAP_t         [31:0] next_arch_map
+  input  ROB_ARCH_MAP_OUT_t                             ROB_Arch_Map_out,
+  output logic              [31:0][$clog2(`NUM_PR)-1:0] next_arch_map
 `endif
 );
 
-  ARCH_MAP_t [31:0] arch_map;
+  logic [31:0][$clog2(`NUM_PR)-1:0] arch_map;
 `ifdef SYNTH_TEST
-  ARCH_MAP_t [31:0] next_arch_map;
+  logic [31:0][$clog2(`NUM_PR)-1:0] next_arch_map;
 `endif
 
   always_comb begin
     next_arch_map = arch_map;
     if (retire_en) begin
-      next_arch_map[ROB_Arch_Map_out.dest_idx].T_idx = ROB_Arch_Map_out.T_idx;
+      next_arch_map[ROB_Arch_Map_out.dest_idx] = ROB_Arch_Map_out.T_idx;
     end // if(rob_archmap_packet.retire_en && en)
   end // always
 
