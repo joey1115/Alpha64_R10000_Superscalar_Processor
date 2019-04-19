ffcc1d658357d13f755fdc013398cd8c4aef3404
brianpur <brianpur@umich.edu>
2019-04-19 03:25:18 -0400

    fixed strange delay problem

--- verilog/Dcache/Dcache.v
+++ verilog/Dcache/Dcache.v
@@ -32,7 +32,7 @@ module Dcache(
   //LRU logic
   logic [`NUM_IDX-1:0]               regA, regB, regC, next_regA, next_regB, next_regC;
   logic [`NUM_IDX-1:0][`NUM_WAY-1:0] LRU_bank_sel, prev_LRU_bank_sel;
-  logic [$clog2(`NUM_WAY)-1:0]       LRU_bank_idx;
+  // logic [$clog2(`NUM_WAY)-1:0]       LRU_bank_idx;
   
 
   //bank logic
@@ -99,41 +99,70 @@ module Dcache(
 `ifdef DEBUG
       .cache_bank(cache_bank),
 `endif
-      .evicted_idx(wr1_addr.set_index),
+      // .evicted_idx(wr1_addr.set_index),
       .evicted_addr(evicted_addr),
       .evicted_dirty(evicted_dirty),
       .evicted_valid(evicted_valid),
       .evicted_data(evicted_data)
       );
 
-  always_comb begin
-    rd1_data_out = 0;
-    rd1_hit_out = 0;
-    for(int i = 0 ; i <`NUM_WAY; i++) begin
-      if(rd1_hit[i] & rd1_search) begin
-        rd1_data_out = rd1_data[i];
-        rd1_hit_out = 1;
-      end
-    end
-  end
 
-  always_comb begin
-    wr1_hit_out = 0;
-    for(int i = 0 ; i <`NUM_WAY; i++) begin
-      if(wr1_hit[i] & wr1_search) begin
-        wr1_hit_out = 1;
-      end
-    end
-  end
+  assign rd1_hit_out = rd1_hit[0] | rd1_hit[1] | rd1_hit[2] | rd1_hit[3];
 
-  assign evicted_dirty_out = evicted_dirty[LRU_bank_idx];
-  assign evicted_valid_out = evicted_valid[LRU_bank_idx];
-  assign evicted_addr_out = evicted_addr[LRU_bank_idx];
-  assign evicted_data_out = evicted_data[LRU_bank_idx];
+  assign rd1_data_out = (rd1_search & rd1_hit[0] & !rd1_hit[1] & !rd1_hit[2] & !rd1_hit[3]) ? rd1_data[0] :
+                        (rd1_search & !rd1_hit[0] & rd1_hit[1] & !rd1_hit[2] & !rd1_hit[3]) ? rd1_data[1] :
+                        (rd1_search & !rd1_hit[0] & !rd1_hit[1] & rd1_hit[2] & !rd1_hit[3]) ? rd1_data[2] :
+                        (rd1_search & !rd1_hit[0] & !rd1_hit[1] & !rd1_hit[2] & rd1_hit[3]) ? rd1_data[3] : 64'hDEADDEADDEADDEAD;
+
+  assign wr1_hit_out = wr1_hit[0] | wr1_hit[1] | wr1_hit[2] | wr1_hit[3];
+
+  // always_comb begin
+  //   rd1_data_out = 0;
+  //   for(int i = 0 ; i <`NUM_WAY; i++) begin
+  //     if(rd1_hit[i] & rd1_search) begin
+  //       rd1_data_out = rd1_data[i];
+  //     end
+  //   end
+  // end
+
+  // always_comb begin
+  //   wr1_hit_out = 0;
+  //   for(int i = 0 ; i <`NUM_WAY; i++) begin
+  //     if(wr1_hit[i] & wr1_search) begin
+  //       wr1_hit_out = 1;
+  //     end
+  //   end
+  // end
+  assign evicted_dirty_out = (wr1_search & LRU_bank_sel[wr1_addr.set_index] == 4'b0001) ? evicted_dirty[0] :
+                             (wr1_search & LRU_bank_sel[wr1_addr.set_index] == 4'b0010) ? evicted_dirty[1] :
+                             (wr1_search & LRU_bank_sel[wr1_addr.set_index] == 4'b0100) ? evicted_dirty[2] :
+                             (wr1_search & LRU_bank_sel[wr1_addr.set_index] == 4'b1000) ? evicted_dirty[3] : 0;
+
+  assign evicted_valid_out = (wr1_search & LRU_bank_sel[wr1_addr.set_index] == 4'b0001) ? evicted_valid[0] :
+                             (wr1_search & LRU_bank_sel[wr1_addr.set_index] == 4'b0010) ? evicted_valid[1] :
+                             (wr1_search & LRU_bank_sel[wr1_addr.set_index] == 4'b0100) ? evicted_valid[2] :
+                             (wr1_search & LRU_bank_sel[wr1_addr.set_index] == 4'b1000) ? evicted_valid[3] : 0;
+
+  assign evicted_addr_out = (wr1_search & LRU_bank_sel[wr1_addr.set_index] == 4'b0001) ? evicted_addr[0] :
+                             (wr1_search & LRU_bank_sel[wr1_addr.set_index] == 4'b0010) ? evicted_addr[1] :
+                             (wr1_search & LRU_bank_sel[wr1_addr.set_index] == 4'b0100) ? evicted_addr[2] :
+                             (wr1_search & LRU_bank_sel[wr1_addr.set_index] == 4'b1000) ? evicted_addr[3] : 0;
+
+  assign evicted_data_out = (wr1_search & LRU_bank_sel[wr1_addr.set_index] == 4'b0001) ? evicted_data[0] :
+                             (wr1_search & LRU_bank_sel[wr1_addr.set_index] == 4'b0010) ? evicted_data[1] :
+                             (wr1_search & LRU_bank_sel[wr1_addr.set_index] == 4'b0100) ? evicted_data[2] :
+                             (wr1_search & LRU_bank_sel[wr1_addr.set_index] == 4'b1000) ? evicted_data[3] : 0;
+
+
+
+  // assign evicted_dirty_out = evicted_dirty[LRU_bank_idx];
+  // assign evicted_valid_out = evicted_valid[LRU_bank_idx];
+  // assign evicted_addr_out = evicted_addr[LRU_bank_idx];
+  // assign evicted_data_out = evicted_data[LRU_bank_idx];
 
   // pe_Dcache rd1_bank_sel (.gnt(rd1_hit),.enc(rd1_hit_idx));
   // pe_Dcache wr1_bank_sel (.gnt(wr1_hit),.enc(wr1_hit_idx));
-  pe_Dcache LRU_bank_sel_mod (.gnt(LRU_bank_sel[wr1_addr.set_index]),.enc(LRU_bank_idx));
+  // pe_Dcache LRU_bank_sel_mod (.gnt(LRU_bank_sel[wr1_addr.set_index]),.enc(LRU_bank_idx));
 
 endmodule
 
@@ -156,7 +185,7 @@ module cache_bank(
   output D_CACHE_LINE_t [`NUM_IDX-1:0]                              cache_bank,
 `endif
   //evicted index to output the address(TAG of the evicted line)
-  input logic [$clog2(`NUM_IDX)-1:0]                                evicted_idx,
+  // input logic [$clog2(`NUM_IDX)-1:0]                                evicted_idx,
   output SASS_ADDR                                                  evicted_addr,
   output logic                                                      evicted_dirty,
   output logic                                                      evicted_valid,
@@ -176,12 +205,12 @@ module cache_bank(
   assign wr1_hit = cache_bank[wr1_addr.set_index].valid && (wr1_addr.tag == cache_bank[wr1_addr.set_index].tag);
 
   //evicted
-  assign evicted_valid = cache_bank[evicted_idx].valid;
-  assign evicted_dirty = cache_bank[evicted_idx].dirty;
-  assign evicted_addr.tag = cache_bank[evicted_idx].tag;
-  assign evicted_addr.set_index = evicted_idx;
+  assign evicted_valid = cache_bank[rd1_addr.set_index].valid;
+  assign evicted_dirty = cache_bank[rd1_addr.set_index].dirty;
+  assign evicted_addr.tag = cache_bank[rd1_addr.set_index].tag;
+  assign evicted_addr.set_index = rd1_addr.set_index;
   assign evicted_addr.ignore = 3'b000;
-  assign evicted_data = cache_bank[evicted_idx].data;
+  assign evicted_data = cache_bank[rd1_addr.set_index].data;
 
   always_comb begin
     next_cache_bank = cache_bank;
@@ -204,31 +233,31 @@ module cache_bank(
         cache_bank[i].tag <= `SD 0;
       end
     else begin
-      cache_bank <= next_cache_bank;
+      cache_bank <= `SD next_cache_bank;
     end
   end
 endmodule
 
-module pe_Dcache(gnt,enc);
-  //synopsys template
-  parameter OUT_WIDTH=$clog2(`NUM_WAY);
-  parameter IN_WIDTH=1<<OUT_WIDTH;
+// module pe_Dcache(gnt,enc);
+//   //synopsys template
+//   parameter OUT_WIDTH=$clog2(`NUM_WAY);
+//   parameter IN_WIDTH=1<<OUT_WIDTH;
 
-  input   [IN_WIDTH-1:0] gnt;
+//   input   [IN_WIDTH-1:0] gnt;
 
-  output [OUT_WIDTH-1:0] enc;
-  wor    [OUT_WIDTH-1:0] enc;
+//   output [OUT_WIDTH-1:0] enc;
+//   wor    [OUT_WIDTH-1:0] enc;
   
-  genvar i,j;
-  generate
-    for(i=0;i<OUT_WIDTH;i=i+1)
-    begin : foo
-      for(j=1;j<IN_WIDTH;j=j+1)
-      begin : bar
-        if (j[i]) begin :if1
-          assign enc[i] = gnt[j];
-        end
-      end
-    end
-  endgenerate
-endmodule
+//   genvar i,j;
+//   generate
+//     for(i=0;i<OUT_WIDTH;i=i+1)
+//     begin : foo
+//       for(j=1;j<IN_WIDTH;j=j+1)
+//       begin : bar
+//         if (j[i]) begin :if1
+//           assign enc[i] = gnt[j];
+//         end
+//       end
+//     end
+//   endgenerate
+// endmodule
