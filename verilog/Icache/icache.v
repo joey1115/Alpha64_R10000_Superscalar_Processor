427b91e91be4f5f3c1421052bb0296402dbf0d98
SPC91 <46912742+SPC91@users.noreply.github.com>
2019-04-19 03:03:08 -0400

    icache still under construction

--- verilog/Icache/icache.v
+++ verilog/Icache/icache.v
@@ -13,31 +13,41 @@ module icache (
   input  logic           [63:0]                          proc2Icache_addr,   // address from f-stage
 
   // Output
-  output logic  [1:0]                                    proc2Imem_command,  // command to memory (BUS_NONE or BUS_LOAD)
-  output logic [63:0]                                    proc2Imem_addr,     // address to memory
+  output logic  [1:0]                                       proc2Imem_command,  // command to memory (BUS_NONE or BUS_LOAD)
+  output logic [63:0]                                       proc2Imem_addr,     // address to memory
 `ifdef DEBUG
-  output I_CACHE_ENTRY_t [`NUM_ICACHE_LINES-1:0]         i_cache,
-  output MEM_TAG_TABLE_t [15:0]                          mem_tag_table,
-  output logic           [$clog2(`NUM_ICACHE_LINES)-1:0] head,
-  output logic           [$clog2(`NUM_ICACHE_LINES)-1:0] tail,
+  output I_CACHE_ENTRY_t [`NUM_ICACHE_LINES-1:0]            i_cache,
+  output MEM_TAG_TABLE_t [15:0]                             mem_tag_table,
+  output logic           [$clog2(`NUM_ICACHE_LINES)-1:0]    head,
+  output logic           [$clog2(`NUM_ICACHE_LINES)-1:0]    tail,
+  output logic           [15-$clog2(`NUM_ICACHE_LINES)-3:0] prefetch_addr_tag_idx,
 `endif
-  output logic           [63:0]                          Icache_data_out,    // value is memory[proc2Icache_addr] // to f-stage
-  output logic                                           Icache_valid_out    // when this is high // hit/miss     // to f-stage
+  output logic           [63:0]                             Icache_data_out,    // value is memory[proc2Icache_addr] // to f-stage
+  output logic                                              Icache_valid_out    // when this is high // hit/miss     // to f-stage
 );
 
 `ifndef DEBUG
   I_CACHE_ENTRY_t [`NUM_ICACHE_LINES-1:0]            i_cache;
   MEM_TAG_TABLE_t [15:0]                             mem_tag_table;
-  logic           [$clog2(`NUM_ICACHE_LINES)-1:0]    head,
-  logic           [$clog2(`NUM_ICACHE_LINES)-1:0]    tail,
+  logic           [$clog2(`NUM_ICACHE_LINES)-1:0]    head;
+  logic           [$clog2(`NUM_ICACHE_LINES)-1:0]    tail;
+  logic           [15-$clog2(`NUM_ICACHE_LINES)-3:0] prefetch_addr_tag_idx; // {tag, index}
 `endif
 
-  logic [1:0]  next_proc2Imem_command;
-  logic [63:0] next_proc2Imem_addr;
+  logic           [1:0]                              next_proc2Imem_command;
+  logic           [63:0]                             next_proc2Imem_addr;
+  I_CACHE_ENTRY_t [`NUM_ICACHE_LINES-1:0]            next_i_cache;
+  MEM_TAG_TABLE_t [15:0]                             next_mem_tag_table;
+  logic           [$clog2(`NUM_ICACHE_LINES)-1:0]    next_head;
+  logic           [$clog2(`NUM_ICACHE_LINES)-1:0]    next_tail;
+  logic           [15-$clog2(`NUM_ICACHE_LINES)-3:0] next_prefetch_addr_tag_idx;
 
   wire [$clog2(`NUM_ICACHE_LINES)-1:0]    idx;       // index of the entry that f-stage requests
   wire [15-$clog2(`NUM_ICACHE_LINES)-3:0] tag;       // tag of the address from f-stage
   wire                                    tag_match; // whether address from f-stage matches the tag in i_cache entry
+  wire                                    write_en;  // write_en for i_cache
+  wire [$clog2(`NUM_ICACHE_LINES)-1:0]    tail_plus_one; // tail+1
+  wire [63:0]                             prefetch_addr; // prefetching address
 
   // 1. f-stage gets inst from i_cache
   assign idx = proc2Icache_addr[3+$clog2(`NUM_ICACHE_LINES)-1:3];
@@ -48,10 +58,33 @@ module icache (
   assign Icache_valid_out = (i_cache[idx].valid && tag_match);
 
   // 2. i_cache request inst from mem
+  assign prefetch_addr = {48'h0, prefetch_addr_tag_idx, 3'h0};
+  assign tail_plus_one = tail + {($clog2(`NUM_ICACHE_LINES)-1){1'b0}, 1'b1};
+  assign next_proc2Imem_command = ((tag_match) && (tail_plus_one == head)) ? BUS_NONE : BUS_LOAD;
+  assign next_proc2Imem_addr    = (tag_match) ? prefetch_addr : proc2Icache_addr;
 
-  
+  // Next prefetch_addr_tag_idx
+  always_comb begin
+    if (!tag_match) begin
+      next_prefetch_addr_tag_idx = {tag, idx};
+    end else begin
+      next_prefetch_addr_tag_idx = prefetch_addr_tag_idx + {{(15-$clog2(`NUM_ICACHE_LINES)-3){1'b0}}, 1'b1};
+    end
+  end
 
-  always_ff begin
+  // Next head and tail
+  assign next_head = idx;
+  always_comb begin
+    if (!tag_match) begin
+      next_tail = idx;
+    end else if ((tail_plus_one != head) && (Imem2proc_response != 4'b0)) begin
+      next_tail = tail_plus_one;
+    end else begin
+      next_tail = tail;
+    end
+  end
+
+  always_ff @(posedge clock) begin
     if (reset) begin
       proc2Imem_command <= `SD BUS_NONE;
       proc2Imem_addr    <= `SD 64'b0;
@@ -62,14 +95,66 @@ module icache (
   end
 
   // 3. i_cache receives inst from mem
+  assign write_en = (Imem2proc_tag != 0) && (mem_tag_table[Imem2proc_tag].valid);
+  always_comb begin
+    next_i_cache = i_cache;
+    // Mem responses to the request
+    if (Imem2proc_response != 4'b0) begin
+      next_i_cache[idx].valid = `FALSE;
+      next_i_cache[idx].tag   = tag;
+    end
+    // Write inst from mem to i_cache
+    if (write_en) begin
+      next_i_cache[mem_tag_table[Imem2proc_tag].idx].valid = `TRUE;
+      next_i_cache[mem_tag_table[Imem2proc_tag].idx].data  = Imem2proc_data;
+    end
+  end
 
+  always_comb begin
+    next_mem_tag_table = mem_tag_table;
+    // Mem responses to the request
+    if (Imem2proc_response != 4'b0) begin
+      next_mem_tag_table[Imem2proc_response].valid = `TRUE;
+      next_mem_tag_table[Imem2proc_response].idx   = idx;
+      next_mem_tag_table[Imem2proc_response].tag   = tag;
+    end
+    // Write inst from mem to i_cache
+    if (write_en) begin
+      next_mem_tag_table[Imem2proc_tag].valid = `FALSE;
+    end
+  end
 
+  always_ff @(posedge clock) begin
+    if (reset) begin
+      i_cache <= `SD 0;
+      head    <= `SD 0;
+      tail    <= `SD 0;
+    end else begin
+      i_cache <= `SD next_i_cache;
+      head    <= `SD next_head;
+      tail    <= `SD next_tail;
+    end
+  end
 
+  always_ff @(posedge clock) begin
+    if (reset) begin
+      mem_tag_table <= `SD 0;
+    end else begin
+      mem_tag_table <= `SD next_mem_tag_table;
+    end
+  end
 
-//   logic                                              valid, write;
-//   I_CACHE_ENTRY_t [`NUM_ICACHE_LINES-1:0]            next_i_cache;
+  always_ff @(posedge clock) begin
+    if (reset) begin
+      prefetch_addr_tag_idx <= `SD 0;
+    end else begin
+      prefetch_addr_tag_idx <= `SD next_prefetch_addr_tag_idx;
+    end
+  end
+
+//   logic                                              valid;
 //   logic           [$clog2(`NUM_ICACHE_LINES)-1:0]    next_head, next_tail, diff1, diff2, tail_plus_one;
-//   MEM_TAG_TABLE_t [15:0]                             next_mem_tag_table;
+//   
 //   logic           [1:0]                              next_proc2Imem_command;
 
 //   assign tail_plus_one          = tail + 1;
@@ -78,7 +163,6 @@ module icache (
 //   assign valid                  = diff1 <= diff2;
 //   assign tag_match                  = i_cache[idx].tag == tag;
 //   assign next_proc2Imem_command = BUS_LOAD;
-//   assign write                  = Imem2proc_tag != 0 && mem_tag_table[Imem2proc_tag].tag == i_cache[mem_tag_table[Imem2proc_tag].idx].tag && mem_tag_table[Imem2proc_tag].valid;
 
 //   always_comb begin
 //     if (tag_match) begin
@@ -118,7 +202,7 @@ module icache (
 //     if (~tag_match) begin
 //       next_i_cache[idx].valid = `FALSE;
 //     end
-//     if (write) begin
+//     if (write_en) begin
 //       next_i_cache[mem_tag_table[Imem2proc_tag].idx].valid = `TRUE;
 //       next_i_cache[mem_tag_table[Imem2proc_tag].idx].data  = Imem2proc_data;
 //     end
@@ -126,7 +210,7 @@ module icache (
 
 //   always_comb begin
 //     next_mem_tag_table = mem_tag_table;
-//     if (write) begin
+//     if (write_en) begin
 //         next_mem_tag_table[Imem2proc_tag].valid = `FALSE;
 //     end
 //     if (tag_match) begin
