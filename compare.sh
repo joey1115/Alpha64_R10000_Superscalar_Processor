244d1c2f3e54c5f9a7bf0d9993d3eb5f946690a3
zjoey <wzhewei@umich.edu>
2019-04-21 23:45:12 -0400

    Update compare.sh

--- compare.sh
+++ compare.sh
@@ -21,6 +21,8 @@ cd project4_provided
 echo "Running make"
 make nuke &> /dev/null
 make simv &> compile.out
+chmod 755 ./vs-asm
+chmod +X ./vs-asm
 for i in ./../test_progs/*.s
 do
   echo "Running $i"
@@ -40,6 +42,8 @@ echo "Running test programs"
 echo "Running make"
 make nuke &> /dev/null
 make simv &> compile.out
+chmod 755 ./vs-asm
+chmod +X ./vs-asm
 for i in ./test_progs/*.s
 do
   echo "Running $i"
